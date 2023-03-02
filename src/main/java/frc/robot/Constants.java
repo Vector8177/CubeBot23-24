70aafdcb956e173cc4910be3d1c3cfc6392d129d
ZaraSofia <112798040+ZaraSofia@users.noreply.github.com>
2023-03-01 22:12:18 -0600

    Add tunable class
    The tunnable class is used to get value from dashboard in tuning mode. :)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -102,11 +102,7 @@ public final class Constants {
       public static final int driveMotorID = 20;
       public static final int angleMotorID = 10;
       public static final int canCoderID = 30;
-<<<<<<< Updated upstream
-      public static final Rotation2d angleOffset = Rotation2d.fromDegrees(238.1835);
-=======
       public static final Rotation2d angleOffset = Rotation2d.fromDegrees(-121.6406);
->>>>>>> Stashed changes
       public static final SwerveModuleConstants constants = new SwerveModuleConstants(driveMotorID, angleMotorID,
           canCoderID, angleOffset);
     }
@@ -116,11 +112,7 @@ public final class Constants {
       public static final int driveMotorID = 21;
       public static final int angleMotorID = 11;
       public static final int canCoderID = 31;
-<<<<<<< Updated upstream
-      public static final Rotation2d angleOffset = Rotation2d.fromDegrees(36.8262);
-=======
       public static final Rotation2d angleOffset = Rotation2d.fromDegrees(110.3906);
->>>>>>> Stashed changes
       public static final SwerveModuleConstants constants = new SwerveModuleConstants(driveMotorID, angleMotorID,
           canCoderID, angleOffset);
     }
@@ -130,11 +122,7 @@ public final class Constants {
       public static final int driveMotorID = 22;
       public static final int angleMotorID = 12;
       public static final int canCoderID = 32;
-<<<<<<< Updated upstream
-      public static final Rotation2d angleOffset = Rotation2d.fromDegrees(345.2);
-=======
       public static final Rotation2d angleOffset = Rotation2d.fromDegrees(52.6464);
->>>>>>> Stashed changes
       public static final SwerveModuleConstants constants = new SwerveModuleConstants(driveMotorID, angleMotorID,
           canCoderID, angleOffset);
     }
@@ -144,11 +132,7 @@ public final class Constants {
       public static final int driveMotorID = 23;
       public static final int angleMotorID = 13;
       public static final int canCoderID = 33;
-<<<<<<< Updated upstream
-      public static final Rotation2d angleOffset = Rotation2d.fromDegrees(205.7519);
-=======
       public static final Rotation2d angleOffset = Rotation2d.fromDegrees(-120.76171875);
->>>>>>> Stashed changes
       public static final SwerveModuleConstants constants = new SwerveModuleConstants(driveMotorID, angleMotorID,
           canCoderID, angleOffset);
     }
