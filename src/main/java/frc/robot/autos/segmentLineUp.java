7346f94840d6a64cc612ea73e21c5ff9627f6090
Murad <agariomadman@gmail.com>
2023-03-19 20:58:00 -0500

    Added human player line up

--- src/main/java/frc/robot/autos/segmentLineUp.java
+++ src/main/java/frc/robot/autos/segmentLineUp.java
@@ -6,6 +6,7 @@ import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.PathPoint;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import frc.robot.Constants;
@@ -18,65 +19,66 @@ public class segmentLineUp {
      * @param segment    the segment TODO
      * @param startPoint
      */
-    public static PathPlannerTrajectory getTrajectory(Constants.SEGMENT segment, Supplier<PathPoint> startPoint) {
-        PathPoint lineUpPoint = startPoint.get();
+    public static PathPlannerTrajectory getTrajectory(Constants.SEGMENT segment, Supplier<Pose2d> startPose) {
+
+        Translation2d lineUpTranslation = startPose.get().getTranslation();
+        Rotation2d lineUpRotation = startPose.get().getRotation();
 
         switch (segment) {
             case CONE_1:
-                lineUpPoint = new PathPoint(
-                        new Translation2d(1.98, 4.93),
-                        Rotation2d.fromDegrees(0), Rotation2d.fromDegrees(180));
+                lineUpTranslation = new Translation2d(1.98, 4.93);
+                lineUpRotation = Rotation2d.fromDegrees(180);
                 break;
             case CONE_2:
-                lineUpPoint = new PathPoint(
-                        new Translation2d(1.98, 3.89),
-                        Rotation2d.fromDegrees(0), Rotation2d.fromDegrees(180));
+                lineUpTranslation = new Translation2d(1.98, 3.89);
+                lineUpRotation = Rotation2d.fromDegrees(180);
                 break;
             case CONE_3:
-                lineUpPoint = new PathPoint(
-                        new Translation2d(1.98, 3.25),
-                        Rotation2d.fromDegrees(0), Rotation2d.fromDegrees(180));
+                lineUpTranslation = new Translation2d(1.98, 3.25);
+                lineUpRotation = Rotation2d.fromDegrees(180);
                 break;
             case CONE_4:
-                lineUpPoint = new PathPoint(
-                        new Translation2d(1.98, 2.2),
-                        Rotation2d.fromDegrees(0), Rotation2d.fromDegrees(180));
+                lineUpTranslation = new Translation2d(1.98, 2.2);
+                lineUpRotation = Rotation2d.fromDegrees(180);
                 break;
             case CONE_5:
-                lineUpPoint = new PathPoint(
-                        new Translation2d(1.98, 1.6),
-                        Rotation2d.fromDegrees(0), Rotation2d.fromDegrees(180));
+                lineUpTranslation = new Translation2d(1.98, 1.6);
+                lineUpRotation = Rotation2d.fromDegrees(180);
                 break;
             case CONE_6:
-                lineUpPoint = new PathPoint(
-                        new Translation2d(1.98, .47),
-                        Rotation2d.fromDegrees(0), Rotation2d.fromDegrees(180));
+                lineUpTranslation = new Translation2d(1.98, .47);
+                lineUpRotation = Rotation2d.fromDegrees(180);
                 break;
             case CUBE_1:
-                lineUpPoint = new PathPoint(
-                        new Translation2d(1.98, 4.43),
-                        Rotation2d.fromDegrees(0), Rotation2d.fromDegrees(180));
+                lineUpTranslation = new Translation2d(1.98, 4.43);
+                lineUpRotation = Rotation2d.fromDegrees(180);
                 break;
             case CUBE_2:
-                lineUpPoint = new PathPoint(
-                        new Translation2d(1.98, 2.74),
-                        Rotation2d.fromDegrees(0), Rotation2d.fromDegrees(180));
+                lineUpTranslation = new Translation2d(1.98, 2.74);
+                lineUpRotation = Rotation2d.fromDegrees(180);
                 break;
             case CUBE_3:
-                lineUpPoint = new PathPoint(
-                        new Translation2d(1.98, 1.05),
-                        Rotation2d.fromDegrees(0), Rotation2d.fromDegrees(180));
+                lineUpTranslation = new Translation2d(1.98, 1.05);
+                lineUpRotation = Rotation2d.fromDegrees(180);
                 break;
             case HUMANPLAYER:
-                lineUpPoint = new PathPoint(
-                        new Translation2d(14.40, 7.59),
-                        Rotation2d.fromDegrees(90), Rotation2d.fromDegrees(90));
+                lineUpTranslation = new Translation2d(14.40, 7.59);
+                lineUpRotation = Rotation2d.fromDegrees(90);
                 break;
         }
 
+        Rotation2d startHeading = new Translation2d(lineUpTranslation.getX(), lineUpTranslation.getY())
+                .minus(startPose.get().getTranslation()).getAngle();
+        Rotation2d endHeading = new Translation2d(lineUpTranslation.getX(), lineUpTranslation.getY())
+                .minus(startPose.get().getTranslation()).getAngle();
+
+        PathPoint startPoint = new PathPoint(startPose.get().getTranslation(), startHeading,
+                startPose.get().getRotation());
+        PathPoint lineUpPoint = new PathPoint(lineUpTranslation, endHeading, lineUpRotation);
+
         return PathPlanner.generatePath(
                 Constants.Autonomous.constraints,
-                startPoint.get(),
+                startPoint,
                 lineUpPoint);
     }
 }
