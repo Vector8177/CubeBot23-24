b36396c686b18f0dcd1591381fa0447c8e0ee46e
Murad <68204671+kidskoding@users.noreply.github.com>
2023-04-03 15:35:45 -0500

    Updated build constants

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Velocity";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 297;
-  public static final String GIT_SHA = "c86c8591f338a9744efe697ba0bad1afe11e20f7";
-  public static final String GIT_DATE = "2023-04-02 03:13:22 EDT";
+  public static final int GIT_REVISION = 300;
+  public static final String GIT_SHA = "0ed9e2221cd50ea4d2e8d103c0d01cbe231ab5f4";
+  public static final String GIT_DATE = "2023-04-03 16:24:59 EDT";
   public static final String GIT_BRANCH = "Logging";
-  public static final String BUILD_DATE = "2023-04-02 22:28:27 EDT";
-  public static final long BUILD_UNIX_TIME = 1680488907965L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2023-04-03 16:33:56 EDT";
+  public static final long BUILD_UNIX_TIME = 1680554036584L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
