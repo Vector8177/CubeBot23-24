1254fecbce367e9987b4b3bd7287e16c695d7e76
Murad <68204671+kidskoding@users.noreply.github.com>
2023-04-13 15:53:39 -0500

    Updated Pathplanner pid

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Velocity";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 320;
-  public static final String GIT_SHA = "f333b919f5dbb90dadc20da2f1f3f656556c608e";
-  public static final String GIT_DATE = "2023-04-08 09:33:53 EDT";
+  public static final int GIT_REVISION = 330;
+  public static final String GIT_SHA = "06993b39c37bdb735fbe56b47206c65ecf924f78";
+  public static final String GIT_DATE = "2023-04-13 03:53:37 EDT";
   public static final String GIT_BRANCH = "Logging";
-  public static final String BUILD_DATE = "2023-04-12 16:49:17 EDT";
-  public static final long BUILD_UNIX_TIME = 1681332557380L;
+  public static final String BUILD_DATE = "2023-04-13 16:49:30 EDT";
+  public static final long BUILD_UNIX_TIME = 1681418970064L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
